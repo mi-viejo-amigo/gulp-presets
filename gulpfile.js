333394af0a7b1b896b36c5842c027e69798302a2
FROL <nkt.frlv7@yandex.ru>
2024-02-10 22:15:38 +0100

    настроен PostCSS с некоторыми плагинами

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ const concat = require('gulp-concat-css');
 const plumber = require('gulp-plumber');
 const del = require('del');
 const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mediaquery = require('postcss-combine-media-query');
+const cssnano = require('cssnano');
+const htmlMinify = require('html-minifier');
 
 //  Функция просмотра через сервер в реальном времени. в остальные функции html css images надо добавить ПАЙП ".pipe(browserSync.reload({stream: true}));"" что бы это выполнялось.
 function serve() {
@@ -16,17 +21,38 @@ function serve() {
 
 //создание папки dist и копирование html в нее.
 function html() {
-  return gulp.src('src/**/*.html')
-        .pipe(plumber())
-                .pipe(gulp.dest('dist/'))
-                .pipe(browserSync.reload({stream: true})); 
+  const options = {
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+      minifyCSS: true,
+      keepClosingSlash: true
+  };
+return gulp.src('src/**/*.html')
+      .pipe(plumber())
+              .on('data', function(file) {
+            const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
+            return file.contents = buferFile
+          })
+              .pipe(gulp.dest('dist/'))
+      .pipe(browserSync.reload({stream: true}));
 }
 
 // склеивание css файла и заливаем все в dist Бандлим. создаем бандл
 function css() {
+  const plugins = [
+    autoprefixer(),
+    mediaquery(),
+    cssnano()
+];
   return gulp.src('src/**/*.css')
         .pipe(plumber())
         .pipe(concat('bundle.css'))
+        .pipe(postcss(plugins))
                 .pipe(gulp.dest('dist/'))
                 .pipe(browserSync.reload({stream: true}));
 }
@@ -35,6 +61,12 @@ function css() {
 function images() {
   return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
             .pipe(gulp.dest('dist/images'))
+            .pipe(browserSync.reload({stream: true}));
+}
+
+function videos() {
+  return gulp.src('src/videos/**/*.{mp4,ogg,mov,avi,flv}')
+            .pipe(gulp.dest('dist/videos'))
             .pipe(browserSync.reload({stream: true}));
 }
 
@@ -48,17 +80,19 @@ function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/**/*.css'], css);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/videos/**/*.{mp4,ogg,mov,avi,flv}'], videos);
 }
 
 // команда объединяющая все 4. gulp.series() объединяет их в одну команду. 
 // а gulp.parallel() говорит что clean будет выполняться первая, а html css и images выполнятся вместе паралельно
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, videos));
 
 // Объединяем ее в паралель с задачей Build и будем вызывать сборку если наблюдатель заметил изменения!
 const watchapp = parallel(build, watchFiles, serve);
 
 exports.clean = clean;
-exports.images = images; 
+exports.images = images;
+exports.videos = videos;
 exports.css = css; 
 exports.html = html; 
 
